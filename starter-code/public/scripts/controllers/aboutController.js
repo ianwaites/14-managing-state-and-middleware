cbaf82d47faa5ae6790ae8de542af649a58c54d9
Ian Waites <ian.waites@gmail.com>
2017-07-28 11:02:52 -0700

    switching drivers

--- starter-code/public/scripts/controllers/aboutController.js
+++ starter-code/public/scripts/controllers/aboutController.js
@@ -5,7 +5,9 @@ var app = app || {};
   const aboutController = {};
 
   // COMMENT: What is this function doing? Where is it called? Does it call any other functions, and if so, in what file(s) do those function(s) live?
-  // (put your response in a comment here)
+  // aboutController.index is an arrow function that holds the logic for showing the #about section of the index.html page while hiding its sibling (the #articles section)
+  // app.repos.requestRepos lives in repo.js and is showing the repos from the site owners GitHub repositories
+  // app.repoView.index lives in repoView.js is getting the data from requestRepos and rendering the data with the repoView function
   aboutController.index = () => {
     $('#about').show().siblings().hide();
     app.repos.requestRepos(app.repoView.index);
--- starter-code/public/scripts/views/adminView.js
+++ starter-code/public/scripts/views/adminView.js
@@ -6,6 +6,7 @@ var app = app || {};
     initAdminPage : () => {
       let template = Handlebars.compile($('#author-template').text());
         // COMMENT: What is this function doing? Where is it called? Does it call any other functions, and if so, in what file(s) do those function(s) live?
+        // this function is taking in the objects within the numWordsByAuthor function (the author name and word count) and then appending the template for the authors stats by targeting the specific IDs using .text to append the number of articles and the number of words in each article
       app.Article.numWordsByAuthor().forEach(stat => $('.author-stats').append(template(stat)));
       $('#blog-stats .articles').text(app.Article.all.length);
       $('#blog-stats .words').text(app.Article.numWordsAll());
